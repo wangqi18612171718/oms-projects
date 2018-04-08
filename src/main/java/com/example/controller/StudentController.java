540b5bbdb529dc6e2e7ba6cb5a4a5b68ec669784
wangqi948570556 <wangqi@mittop.com>
2018-04-08 18:09:59 +0800

    spring boot 增加jsp

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -3,27 +3,33 @@ package com.example.controller;
 import com.example.model.Student;
 import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by wangqi on 18/4/2.
  */
-@RestController
+@Controller
 public class StudentController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/hello")
-    public String hello(){
-        return "hello how are you ! ! !";
+    @RequestMapping("/test")
+    public String hello(Model model){
+        System.out.print("fsa");
+        model.addAttribute("test","测试一下看看");
+        return "views/index";
     }
 
     @RequestMapping("getUser")
-    public String getUser(){
+    public String getUser(Model model){
         Student student = userService.findById(1);
-        System.out.println(student.getName());
-        return "用户名:"+student.getName()+"用户ID:"+student.getId();
+        model.addAttribute("Name",student.getName());
+        model.addAttribute("Id",student.getId());
+        return "views/index";
     }
 }
